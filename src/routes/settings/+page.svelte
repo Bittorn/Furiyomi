f882399c2f22fd3d313e5d46ef5e330334235d52
Bittorn <48916178+Bittorn@users.noreply.github.com>
2026-03-18 16:33:54 +1100

    i hate this code, but IT SORT OF WORKS SO IDGAF!

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,17 +1,42 @@
 <script lang='ts'>
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     import styles from './settings.module.scss';
+
+    let form: HTMLFormElement;
+
+    function updatePreferences(event: Event) {
+        event.preventDefault();
+        if (event.target && typeof window !== 'undefined') {
+            // @ts-expect-error Furigana will show as not existing, but we know it does
+            let furigana: string = event.target.furigana.value;
+            console.log(furigana)
+            localStorage.furigana = furigana;
+        }
+    }
+
+    function init() {
+        let furigana: string | null = localStorage.getItem("furigana");
+
+        if (furigana) {
+            console.log(furigana)
+            form[furigana].checked = true;
+        } else {
+            localStorage.setItem("furigana", "all");
+        }
+    }
 </script>
 
 <svelte:head>
     <title>Settings | Furiyomi</title>
 </svelte:head>
 
+<svelte:window on:load={init}></svelte:window>
+
 <h1>Settings</h1>
 
-<form onsubmit={console.log}>
+<form bind:this={form} method="post" onsubmit={updatePreferences}>
     <h2>Show furigana for...</h2>
-    <input type="radio" id="all" name="furigana" value="all" defaultChecked>
+    <input type="radio" id="all" name="furigana" value="all">
     <label for="all">All kanji</label><br>
     <input type="radio" id="n4" name="furigana" value="n4">
     <label for="n4">N4 kanji and higher</label><br>
