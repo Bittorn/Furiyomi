f0f1d4d8367b3b88075ffa5f75da75639c6c81da
Bittorn <48916178+Bittorn@users.noreply.github.com>
2026-03-20 23:53:11 +1100

    Revert "remove the bull"
    This reverts commit 00f5469c9d6c41c50ecd385f9605a37f08fddafd.

---
+++ src/routes/discover/lonely-girl-ni-sakaraenai/+page.svelte
@@ -0,0 +1,25 @@
+<!-- eslint-disable svelte/no-navigation-without-resolve -->
+<script lang="ts">
+	import styles from './page.module.scss';
+	import cover from './1/vol1/0.jpeg';
+</script>
+
+<svelte:head>
+	<title>Lonely Girl ni Sakaraenai - Furiyomi</title>
+</svelte:head>
+
+<h1>Lonely Girl ni Sakaraenai</h1>
+
+<img src={cover} alt="Manga cover" class={styles.cover} />
+
+<br />
+
+<!-- eslint-disable svelte/no-navigation-without-resolve -->
+<p>
+	<a href="vol1.html">Read volume 1</a><br />
+	<a href="vol2.html">Read volume 2</a><br />
+	<a href="vol3.html">Read volume 3</a><br />
+	<a href="vol4.html">Read volume 4</a><br />
+	<a href="vol5.html">Read volume 5</a><br />
+	<a href="vol6.html">Read volume 6</a><br />
+</p>
