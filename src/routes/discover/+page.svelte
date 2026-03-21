71a2f00f51a5927a21c8fb3e4640d761b0a5b4aa
Bittorn <48916178+Bittorn@users.noreply.github.com>
2026-03-21 23:08:19 +1100

    NEW MANGA YIPPEE !!

--- src/routes/discover/+page.svelte
+++ src/routes/discover/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import styles from './discover.module.scss';
 	import lonelyGirlCover from '$lib/assets/covers/lonely-girl-ni-sakaraenai.jpeg';
+	import whichIsLoveCover from '$lib/assets/covers/dore-ga-koi-ka-ga-wakaranai.jpg';
+	import wantToTouchCover from '$lib/assets/covers/furetai-wa-koi-no-hajimari.jpeg';
 </script>
 
 <svelte:head>
@@ -17,11 +19,36 @@
 <div class={styles.manga}>
 	<div>
 		<h2>
+			どれが<ruby>恋<rt>こい</rt></ruby>かがわからない
+		</h2>
+		<h3>I Don’t Know Which is Love</h3>
+		<img src={whichIsLoveCover} alt="Manga cover" class={styles.cover} /><br />
+		<!-- eslint-disable-next-line svelte/no-navigation-without-resolve -->
+		<a href="discover/dore-ga-koi-ka-ga-wakaranai">View</a>
+		<br /><br />
+	</div>
+
+	<div>
+		<h2>
+			「<ruby>触<rt>ふ</rt></ruby>れたい」は<ruby>恋<rt>こい</rt></ruby>の<ruby
+				>始<rt>はじ</rt></ruby
+			>まり
+		</h2>
+		<h3>"I Want to Touch" is How Love Starts</h3>
+		<img src={wantToTouchCover} alt="Manga cover" class={styles.cover} /><br />
+		<!-- eslint-disable-next-line svelte/no-navigation-without-resolve -->
+		<a href="discover/furetai-wa-koi-no-hajimari">View</a>
+		<br /><br />
+	</div>
+
+	<div>
+		<h2>
 			ロンリーガールに<ruby>逆<rt>さか</rt></ruby>らえない
 		</h2>
 		<h3>I Can't Say No to the Lonely Girl</h3>
 		<img src={lonelyGirlCover} alt="Manga cover" class={styles.cover} /><br />
 		<!-- eslint-disable-next-line svelte/no-navigation-without-resolve -->
 		<a href="discover/lonely-girl-ni-sakaraenai">View</a>
+		<br /><br />
 	</div>
 </div>
---
+++ src/routes/discover/furetai-wa-koi-no-hajimari/+page.svelte
@@ -0,0 +1,44 @@
+<!-- eslint-disable svelte/no-navigation-without-resolve -->
+<script lang="ts">
+	import styles from '../discover.module.scss';
+	import cover from '$lib/assets/covers/furetai-wa-koi-no-hajimari.jpeg';
+</script>
+
+<svelte:head>
+	<title>「触れたい」は恋の始まり - Furiyomi</title>
+</svelte:head>
+
+<h1>
+	「<ruby>触<rt>ふ</rt></ruby>れたい」は<ruby>恋<rt>こい</rt></ruby>の<ruby>始<rt>はじ</rt></ruby
+	>まり
+</h1>
+<h2>"I Want to Touch" is How Love Starts</h2>
+
+<!-- eslint-disable svelte/no-navigation-without-resolve -->
+<a href="/discover" class={styles.back}>Back</a><br /><br /><br />
+
+<img src={cover} alt="Manga cover" class={styles.solocover} />
+
+<div class={styles.links}>
+	<a href="https://anilist.co/manga/181754/Furetai-wa-Koi-no-Hajimari/" class={styles.back}
+		>View on AniList</a
+	>
+</div>
+
+<div class={styles.description}>
+	<p>
+		With her cool demeanor and refined looks, the well-read Kaede Minato is revered by her
+		schoolmates. Momoko Hattori’s cute appearance made her a bit of an idol in the school right
+		after enrollment. The two had virtually no interaction when Momoko’s chance encounter with Kaede
+		promptly accelerates their relationship. A romantic tale on the brink of love.
+		<i>(Source: Takeshobo, translated)</i>
+	</p>
+</div>
+
+<br />
+
+<!-- eslint-disable svelte/no-navigation-without-resolve -->
+<div class={styles.volumes} style="grid-template-columns: auto auto;">
+	<a href="/mokuro/furetai-wa-koi-no-hajimari/1/vol1.html">Read volume 1</a>
+	<a href="/mokuro/furetai-wa-koi-no-hajimari/2/vol2.html">Read volume 2</a>
+</div>
