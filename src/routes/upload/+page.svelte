67b67dd540d8501e61ba743bfa8a88b2225dbb70
Bittorn <48916178+Bittorn@users.noreply.github.com>
2026-03-29 18:53:19 +1100

    attempting to fix docker routing

--- src/routes/upload/+page.svelte
+++ src/routes/upload/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
-	import './upload.module.scss';
+	import styles from './upload.module.scss';
+
+	let fileInput: HTMLInputElement;
 </script>
 
 <svelte:head>
@@ -10,10 +12,49 @@
 <h1>Upload</h1>
 
 <form method="post" use:enhance enctype="multipart/form-data">
-	<div>
-		<label for="file">Select file: </label>
-		<input type="file" id="file" name="fileToUpload" webkitdirectory multiple required />
+	<div class={styles.upload}>
+		<label
+			for="file"
+			class={styles.dropcontainer}
+			on:dragover={(e) => e.preventDefault()}
+			on:drop={(e) => {
+				e.preventDefault();
+				fileInput.files = e.dataTransfer!.files;
+			}}
+		>
+			<span class={styles.droptitle}>Drop directory here</span>
+			or
+			<input
+				bind:this={fileInput}
+				type="file"
+				id="file"
+				name="fileToUpload"
+				webkitdirectory
+				multiple
+				required
+			/>
+		</label>
 	</div>
 	<br />
-	<button type="submit">Upload</button>
+	<button class={styles.uploadbutton} type="submit">Upload</button>
 </form>
+
+<div class={styles.information}>
+	<h1>⚠️ Before Uploading ⚠️</h1>
+	<ul>
+		<li>This app is intended for use only with <b>manga pre-processed with Mokuro.</b></li>
+		<li>
+			When uploading, be sure to select the directory .mokuro files are located in, and <b>not</b> any
+			child or parent directories.
+		</li>
+		<li>
+			Ensure the name of the directory matches the manga you are uploading, as this will be used for
+			metadata fetching.
+		</li>
+		<li>
+			Currently, upload progress is not shown. If the page reloads or the input field resets, <b
+				>the upload most likely succeeded.</b
+			>
+		</li>
+	</ul>
+</div>
--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,18 @@
 import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// Output directory for the built server
+			out: 'build',
+			// Listen on all interfaces inside Docker
+			host: '0.0.0.0',
+			port: 3796
+		})
 	}
-	// vitePlugin: {
-	// 	dynamicCompileOptions: ({ filename }) =>
-	// 		filename.includes('node_modules') ? undefined : { runes: true }
-	// }
 };
 
 export default config;
