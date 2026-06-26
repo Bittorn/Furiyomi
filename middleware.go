4420944cd0d0c5f4a6e5f364a0cfbd1fa8e3044f
Bittorn <48916178+Bittorn@users.noreply.github.com>
2026-06-26 19:29:08 +1000

    Update middleware.go

--- middleware.go
+++ middleware.go
@@ -9,7 +9,7 @@ import (
 
 func logRequestMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%s %s [%s]\n", r.Method, r.URL, r.RemoteAddr)
+		log.Printf("[%s] %s %s\n", r.RemoteAddr, r.Method, r.URL)
 
 		next.ServeHTTP(w, r)
 	})
